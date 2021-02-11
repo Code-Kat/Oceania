d0238d648f1cb21ee081e7369161601c3559bd74
Code-Kat <73153064+Code-Kat@users.noreply.github.com>
2021-02-11 20:55:09 +0900

    Finished Home, working on SearchPage

---
+++ src/components/Search.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import "./Search.css";
+import SearchIcon from '@material-ui/icons/Search';
+import { useHistory } from "react-router-dom";
+import { useStateValue } from '../contextAPI/StateProvider';
+import { actionTypes } from '../contextAPI/reducer';
+
+function Search() {
+    const[{}, dispatch] = useStateValue();
+
+    const history = useHistory();
+    const [input, setInput] = useState ('')
+
+
+    const search=e=>{
+        e.preventDefault();
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input,
+        })
+        history.push('/search');
+    }
+    return (
+        <div className="search">     
+            <form>
+                <input 
+                    value={input}
+                    onChange={e=> setInput(e.target.value)}
+                    className="search__input"
+                    placeholder="Search the web to clean our oceans"
+                />
+                <SearchIcon className="search__icon"/> 
+                <button
+                    type="submit" 
+                    onClick={search}></button>
+            </form>   
+        </div>
+    )
+}
+
+export default Search
