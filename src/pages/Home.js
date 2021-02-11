7762b1de7c5e41df1c96c98a777715bf85fbef6b
Code-Kat <73153064+Code-Kat@users.noreply.github.com>
2021-02-11 12:07:42 +0900

    updated README

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,16 @@ function Home() {
     return (
         <div className="home">
             <div className="home__header">
-                <div className="home__headerLeft"></div>
-                <div className="home__headerRight"></div>
+                <div className="home__headerLeft">
+                    {/* Link */}
+                    {/* Link */}
+                </div>
+                <div className="home__headerRight">
+                    {/* Link */}
+                    {/* Link */}
+                    {/* Icon */}
+                    {/* Avatar */}
+                </div>
             </div>
             
             <div className="home__body"></div>
