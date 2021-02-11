bb59674e8290ea2b1b644d0c790b35cdb38e4ba9
Code-Kat <73153064+Code-Kat@users.noreply.github.com>
2021-02-11 12:04:50 +0900

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import "./Home.css";
+
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__header">
+                <div className="home__headerLeft"></div>
+                <div className="home__headerRight"></div>
+            </div>
+            
+            <div className="home__body"></div>
+        </div>
+    )
+}
+
+export default Home;
